a2fe4c476d713ce3e08832721ee457e2219639c6
= <=>
2025-04-23 17:16:22 -0400

    moved stuff to video.py

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
 import video
 
-def get_events(path:str)->dict:
-    """ Opens events file and returns array containing the events """
-    ret = {"max": 0, "events":[], "dim": []} 
-
-    with open(path, encoding='utf-8') as f:
-        dims = f.readline().replace('\n', '').split(" ")
-        ret["dim"].append(int(dims[0]))
-        ret["dim"].append(int(dims[1]))
-        ret["max"] = int(f.readline().replace("\n", '').split(" ")[0])
-
-        for i, line in enumerate(f):
-
-            estring = line.replace('\n', '').split(' ')
-            if len(estring) < 4:
-                continue
-            ret["events"].append({
-                     "x": int(estring[0]),
-                     "y": int(estring[1]),
-                     "p": int(estring[2]),
-                     "t": int(estring[3]),
-                     })
-
-    return ret
-
 if __name__ == "__main__":
-    event_data = get_events('normal.txt')
-    video.create_video(event_data, "output.mp4")
-    event_data = get_events('warped.txt')
-    video.create_video(event_data, "wtf.mp4")
+    video.create_video_from_txt("normal.txt", "out.mp4")
+    video.create_video_from_txt("./warped.txt", "./wtf.mp4")
--- video.py
+++ video.py
@@ -1,7 +1,33 @@
 import cv2
 import numpy as np
 
+def create_video_from_txt(in_path:str, out_path:str):
+    event_data = get_events(in_path)
+    create_video(event_data, out_path)
 
+def get_events(path:str)->dict:
+    """ Opens events file and returns dict containing events and additional data"""
+    ret = {"max": 0, "events":[], "dim": []} 
+
+    with open(path, encoding='utf-8') as f:
+        dims = f.readline().replace('\n', '').split(" ")
+        ret["dim"].append(int(dims[0]))
+        ret["dim"].append(int(dims[1]))
+        ret["max"] = int(f.readline().replace("\n", '').split(" ")[0])
+
+        for i, line in enumerate(f):
+
+            estring = line.replace('\n', '').split(' ')
+            if len(estring) < 4:
+                continue
+            ret["events"].append({
+                     "x": int(estring[0]),
+                     "y": int(estring[1]),
+                     "p": int(estring[2]),
+                     "t": int(estring[3]),
+                     })
+
+    return ret
 
 def create_video(event_data, filename):
     events = event_data["events"]
