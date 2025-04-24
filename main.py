cca85c6c8c8047b62b7e1a292470a43bd0ba2dc5
= <=>
2025-04-24 09:29:18 -0400

    cleaned up example main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import video
 
 if __name__ == "__main__":
-    video.create_video_from_txt("normal.txt", "out.mp4")
+    video.create_video_from_txt("./normal.txt", "out.mp4")
     video.create_video_from_txt("./warped.txt", "./wtf.mp4")
